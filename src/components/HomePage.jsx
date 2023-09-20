ff4d9d8d3640a0611d2bd2200d3921caba6e0954
jonathan person <107312207+Jaypee93@users.noreply.github.com>
2023-09-20 21:42:32 +0300

    Some changes made. New component created and imported to homepage. Homepage working good. MarvelPage Route needs to be fixed. Design changes on hold

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,32 @@
-import { useState} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import MarvelCard from "./MarvelCard";
 import Search from "./Search";
-import './HomePage.css'
+import MarvelBigCard from "./MarvelBigCard";
+import './HomePage.css';
 
 const HomePage = ({data}) => {
-    const [searchInput, setSearchInput] = useState('')
+    const [searchInput, setSearchInput] = useState('');
+    const [randomCharacter, setRandomCharacter] = useState(null);
 
     const handleSearchChange = (input) => {
         setSearchInput(input);
-
     }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Generate a random index within the data array
+            const randomIndex = Math.floor(Math.random() * data.length);
+            setRandomCharacter(data[randomIndex]);
+        }, 8000);
+
+        // Set the initial random character
+        const randomIndex = Math.floor(Math.random() * data.length);
+        setRandomCharacter(data[randomIndex]);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, [data]);
 
     return ( 
         <div className="homepage-container">
@@ -18,8 +34,13 @@ const HomePage = ({data}) => {
             <div className="search-container">
                 <Search handleSearchChange={handleSearchChange}/>
             </div>
+            <div className="homepage-secondcontainer">
             <div className="cards">
                 <MarvelCard data = {data} searchInput={searchInput}/>
+            </div>
+            <div className="bigCard">
+                {randomCharacter && <MarvelBigCard data={randomCharacter} />}
+            </div>
             </div>
         </div>
      );
---
+++ src/components/MarvelBigCard.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const MarvelBigCard = ({ data }) => {
+  const [character, setCharacter] = useState(null);
+  const { name } = useParams();
+
+  useEffect(() => {
+    console.log("data:", data);
+    console.log("name:", name);
+    setCharacter(data);
+  }, [data, name]);
+
+  if (!character) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="MarvelBigCard-container">
+      <img
+        className="marvelpage-image"
+        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+        alt={character.name}
+      />
+      <div className="marvelpage-text">
+        <h1 className="marvelpage-name">{character.name}</h1>
+        <h1 className="marvelpage-description">{character.description}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MarvelBigCard;
